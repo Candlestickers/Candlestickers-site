4800077bae16f8c0d39f00b1434537f2a42df87c
alani25 <alanihamzah25@gmail.com>
2025-10-25 21:58:10 -0400

    switched to dark mode, added sky canvas, placeholder embeds

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+function showIframe(link) {
+        // alert("OPENING IFRAME")
+    const container = document.getElementById('iframe-container');
+    container.innerHTML = `
+        <iframe
+        frameborder="0"
+        src="${link}"
+        allowfullscreen
+        width="720"
+        height="440">
+        </iframe>
+    `
+    container.scrollIntoView({ behavior: 'smooth' })
+    }
