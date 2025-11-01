6378687a1960e3e67b9e23e5f07f13378153c73b
Baron <43506286+BaronAWC@users.noreply.github.com>
2025-11-01 01:51:52 -0700

    partial stars rework
    renamed variables, const parameters, better parallax, more intuitive math (probably)

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import './style.css';
 
 // <1 = normal parallax, 1 = no parallax, >1 = scroll faster than webpage
 const BG_PARALLAX = 2;
-
 const BG_TOP = '#0e012cff'
 const BG_BOTTOM = '#4a0a8eff'
 
+const PARALLAX_MAX = 0.5;
+const STAR_MIN = 1;
+const STAR_MAX = 3;
+const STAR_GROW = 3;
+
 let mouse = { x: 0, y: 0 }; // mouse position (see mousemove listener)
+var distToMouse = (x, y) => Math.sqrt((x - mouse.x) ** 2 + (y - mouse.y) ** 2);
 
 /////////////////////////////
 // BACKGROUND STARS CANVAS //
@@ -22,7 +27,8 @@ for (let i = 0; i < 250; i++) {
     stars.push({
         x: Math.random(),
         y: Math.random(),
-        r: Math.random() * 1.2
+        radius: Math.random() * (STAR_MAX-STAR_MIN) + STAR_MIN,
+        parallax: Math.random() * PARALLAX_MAX
     });
 }
 let meteors = [];
@@ -30,20 +36,20 @@ for (let i = 0; i < 20; i++) {
     meteors.push({
         x: Math.random(),
         y: Math.random(),
-        r: Math.random(),
+        radius: Math.random() * (STAR_MAX-STAR_MIN) + STAR_MIN,
+        parallax: Math.random() * PARALLAX_MAX,
         length: 0
     });
 }
 
 function drawBackground() {
+    // GRADIENT
     var scrollRatio = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
-    var distToMouse = (x, y) => Math.sqrt((x - mouse.x) ** 2 + (y - mouse.y) ** 2);
 
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas 
     var constellation = [];
     
-
     // adding background gradient
     // determine pan amount based on scrolled amount
     let gradientPan = scrollRatio * BG_PARALLAX;
@@ -55,16 +61,23 @@ function drawBackground() {
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // draw stars
+    // STARS
     stars.map((value) => {
         ctx.beginPath();
-        let newHeight = value.y * canvas.height - window.scrollY * value.r; // <-- parallax effect to height
-        let xy = [value.x * canvas.width, newHeight < 0 ? newHeight + canvas.height : newHeight];
+
+        let canvasX = value.x * canvas.width;
+        // add parallax and wrap stars to top and bottom
+        let canvasY = value.y * canvas.height - window.scrollY * value.parallax;
+        while(canvasY < -(value.radius + STAR_GROW)/2) canvasY += canvas.height + value.radius + STAR_GROW; // wrap: ensure star is fully off screen (and also mod doesn't work with negative numbers?)
+
+        let xy = [canvasX, canvasY];
+
+        // draw
         ctx.arc(
             xy[0],
             xy[1],
-            // buncha numbers for simple resize when mouse is near star
-            value.r + Math.min(5, Math.max(1, 70 / Math.sqrt((value.x * canvas.width - mouse.x) ** 2 + ((newHeight > canvas.height ? newHeight - canvas.height : newHeight) - mouse.y) ** 2))),
+            // magic math for stars growing if the mouse is close
+            value.radius + STAR_GROW/(distToMouse(canvasX, canvasY)**2/100 + 1),
             0, Math.PI * 2
         );
         ctx.fillStyle = 'white';
@@ -74,13 +87,14 @@ function drawBackground() {
             constellation.push([...xy, 1 / distToMouse(...xy)]);
     });
 
+    // METEORS
     meteors.forEach((value) => {
 
         const maxL = 200;
         if (scrollRatio > 0.75) {
             value.length += (maxL - value.length) / 52;
         } else {
-            value.length = Math.max(value.r, value.length - 4 || Math.max(1, value.r * 5));
+            value.length = Math.max(value.radius, value.length - 4 || Math.max(1, value.radius * 5));
         }
 
         ctx.beginPath();
@@ -89,11 +103,11 @@ function drawBackground() {
         let pos = {
             x: value.x * canvas.width * 1.4,
             y: document.documentElement.scrollHeight - value.y * canvas.height / 1.4 - (scrollY + 200),
-            r: Math.min(100, value.r * 5 * 1 / (1 / Math.max(scrollRatio, 1e5) - 1))
+            r: Math.min(100, value.radius * 5 * 1 / (1 / Math.max(scrollRatio, 1e5) - 1))
         }
 
         // ctx.stroke = 10;
-        ctx.lineWidth = 1 + value.r;
+        ctx.lineWidth = 1 + value.radius;
         ctx.moveTo(pos.x + maxL / 3, pos.y - maxL / 3);
         ctx.lineTo(pos.x + maxL / 3 - value.length * 2, pos.y - maxL / 3 + value.length * 0.5);
 
@@ -114,7 +128,7 @@ function drawBackground() {
         // ctx.arc(
         //     value.x * canvas.width,
         //     document.documentElement.scrollHeight - value.y * canvas.height/2 - (scrollY + 150),
-        //     value.r * 5,
+        //     value.radius * 5,
         //     0,
         //     Math.PI*2
         //   );
